357253446e35b5f2b33d573fc36e0008cdbcc842
강성찬 <tjdcks_12@naver.com>
2019-05-23 11:33:46 +0900

    BooksReducer 생성

--- ReduxSection3/src/reducers/index.js
+++ ReduxSection3/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_books';
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
+const rootReducer = combineReducers({ // 여기에 스테이트를 매핑한다.
+  books: BooksReducer
+  //books라는 프로퍼티(스테이트 값) 값으로 booksReudcer 이름의 리듀서를 갖는것.
 });
 
 export default rootReducer;
